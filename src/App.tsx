f933a639fe5f90ee9fa2edf6fb9cb21bebee903a
Matt Mattox <bizofmatt@gmail.com>
2026-01-15 22:09:12 -0800

    installed react router and added the first route for our landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Home from "./pages/Home";
-
+import { Routes, Route } from "react-router";
 const App = () => {
 
-
   return (
-<Home />
-  );
-};
+    <Routes>
+       <Route path="/" element={<Home />}></Route>
+    </Routes>
+  )
+}
+
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from "../src/App";
-
 import "../src/index.css";
 
-// 2. Find the root element in index.html
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  // 3. Create a React root and render the App component
-  ReactDOM.createRoot(rootElement).render(
-    // StrictMode helps identify potential problems in an application.
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-    // Optional: Log an error if the root element isn't found
-    console.error('Root element with ID "root" not found in the DOM.');
+const root = document.getElementById('root');
+if(!root) {
+  throw new Error('Failed to find the root element');
 }
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+);
