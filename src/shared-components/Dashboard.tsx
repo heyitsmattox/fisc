728975a74fff907eed22a8626e38e33c72850edc
Matt Mattox <bizofmatt@gmail.com>
2026-01-19 09:33:08 -0800

    form template created on our inventoryForm component

--- src/shared-components/Dashboard.tsx
+++ src/shared-components/Dashboard.tsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
   return (
     <>
     <Navbar showNavbarMenuIcon={true} showFiscImageLogo={false} />
-    
+    <h1>Dashboard component placeholder</h1>
     </>
   );
 };
