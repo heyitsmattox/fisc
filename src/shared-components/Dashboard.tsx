7bed506bd5773bde0fac4d433a143d9b9db4d423
Matt Mattox <bizofmatt@gmail.com>
2026-03-04 20:35:23 -0800

    pulling data from database

--- src/shared-components/Dashboard.tsx
+++ src/shared-components/Dashboard.tsx
@@ -1,14 +1,37 @@
 import Navbar from "./Navbar";
+import { supabase } from "../lib/supabaseClient"
+import { useEffect, useState } from "react";
+import type { Database } from "../lib/database.types";
+
+type InventoryEntry = Database["public"]["Tables"]["inventory"]["Row"];
 
 const Dashboard = () => {
+  const [inventoryData, setInventoryData ] = useState<InventoryEntry[]>([])
+  const [fetchError, setFetchError] = useState<string | null>("");
 
-
-
+useEffect(() => {
+  const fetchData = async () => {
+      const {data, error } = await supabase
+      .from("inventory")
+      .select()
+      if (error) {
+        setFetchError("Could not fetch the data from our inventory database")
+        console.log(error)
+      }
+      if(data) {
+       setInventoryData(data)
+       setFetchError(null)
+      }
+  }
+  fetchData()
+}, [])
+console.log(inventoryData)
 
   return (
     <>
     <Navbar showNavbarMenuIcon={true} showFiscImageLogo={false} />
     <h1>Dashboard component placeholder</h1>
+
     </>
   );
 };
--- src/shared-components/Navbar.d.ts
+++
@@ -1,7 +0,0 @@
-interface NavbarProps {
-    img?: string;
-    menuIcon?: string;
-    showIcon: boolean;
-}
-declare const Navbar: ({ img, menuIcon, showIcon }: NavbarProps) => import("react/jsx-runtime").JSX.Element;
-export default Navbar;
