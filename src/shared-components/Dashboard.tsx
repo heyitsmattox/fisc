f36d1dcee6e88833e7135546e44a2b8cd07d00b4
Matt Mattox <bizofmatt@gmail.com>
2026-03-07 12:20:27 -0800

    added react query functionality in our Card component while removing useEffect logic in our Dashboard component

--- src/shared-components/Dashboard.tsx
+++ src/shared-components/Dashboard.tsx
@@ -2,43 +2,45 @@ import Navbar from "./Navbar";
 import { supabase } from "../lib/supabaseClient"
 import { useEffect, useState } from "react";
 import type { Database } from "../lib/database.types";
+import Card from "./Card";
 
 type InventoryEntry = Database["public"]["Tables"]["inventory"]["Row"];
 
 const Dashboard = () => {
-  const [inventoryData, setInventoryData ] = useState<InventoryEntry[]>([])
-  const [fetchError, setFetchError] = useState<string | null>("");
+  // const [inventoryData, setInventoryData ] = useState<InventoryEntry[]>([])
+  // const [fetchError, setFetchError] = useState<string | null>("");
 
-useEffect(() => {
-  const fetchData = async () => {
-      const {data, error } = await supabase
-      .from("inventory")
-      .select()
-      if (error) {
-        setFetchError("Could not fetch the data from our inventory database")
-        console.log(error)
-      }
-      if(data) {
-       setInventoryData(data)
-       setFetchError(null)
-      }
-  }
-  fetchData()
-}, [])
-console.log('here is our data that we will map through ---->', inventoryData)
+// useEffect(() => {
+//   const fetchData = async () => {
+//       const {data, error } = await supabase
+//       .from("inventory")
+//       .select()
+//       if (error) {
+//         setFetchError("Could not fetch the data from our inventory database")
+//         console.log(error)
+//       }
+//       if(data) {
+//        setInventoryData(data)
+//        setFetchError(null)
+//       }
+//   }
+//   fetchData()
+// }, [])
+//console.log('here is our data that we will map through ---->', inventoryData)
 
-let totalProfit = inventoryData.reduce((acc, entry) => {
-  return acc + (entry.profit ?? 0)
-}, 0)
+// let totalProfit = inventoryData.reduce((acc, entry) => {
+//   return acc + (entry.profit ?? 0)
+// }, 0)
 
-console.log('total profit value --->', totalProfit)
+// console.log('total profit value --->', totalProfit)
 
 
   return (
     <>
     <Navbar showNavbarMenuIcon={true} showFiscImageLogo={false} />
     <h1>Dashboard component placeholder</h1>
-    <div>{totalProfit}</div>
+    {/* <div>{totalProfit}</div> */}
+    <Card />
     
 
     </>
