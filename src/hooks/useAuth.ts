01712df0e25c23d58ae72d65d7b20aa195cb1d1a
Matt Mattox <bizofmatt@gmail.com>
2026-04-23 21:11:30 -0700

    persisted new entries to UI by calling the data from our database to stay rendering in our UI

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -7,6 +7,9 @@ export const useAuth = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    // this line checks if there is an active session when the component mounts. 
+    // If there is, it sets the user state to the current user and sets loading to false. 
+    // If there isn't, it just sets loading to false.
     const initAuth = async () => {
       const {
         data: { session },
@@ -17,14 +20,17 @@ export const useAuth = () => {
 
     initAuth();
 
+    // this listener will update the user state whenver the auth changes. e.g sign in or sign out.
     const { data } = supabase.auth.onAuthStateChange((_event, session) => {
       setUser(session?.user ?? null);
     });
 
+    // tells our code to stop listening to the auth state change to avoid memory leaks
     return () => {
       data?.subscription?.unsubscribe();
     };
   }, []);
 
+  //allows us to utilize the user object and loading state in other components
   return { user, isLoading };
 };
